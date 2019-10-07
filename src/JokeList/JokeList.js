d7a34b704b185cf2215cdc9be1faf50fcfa53cb7
Denise <denise.ndalamba@gmail.com>
2019-10-07 12:47:46 -0400

    initial components

---
+++ src/JokeList/JokeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './JokeList.css'
+import axios from 'axios'
+
+class JokeList extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default JokeList
