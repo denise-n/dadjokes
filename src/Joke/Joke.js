4cca25a18686a323c0eee7db78877e2461793080
Denise <denise.ndalamba@gmail.com>
2019-10-07 12:48:44 -0400

    initial components

---
+++ src/Joke/Joke.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Joke.css'
+
+class Joke extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Joke
