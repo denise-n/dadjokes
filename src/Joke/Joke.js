4d06803bbd228bb96e916f571195a8a383a2f6ff
Denise <denise.ndalamba@gmail.com>
2019-10-08 13:02:45 -0400

    add changing emoji

--- src/Joke/Joke.js
+++ src/Joke/Joke.js
@@ -2,26 +2,66 @@ import React, { Component } from 'react'
 import './Joke.css'
 
 class Joke extends Component {
+    getColour = () => {
+        const {votes} = this.props
+        if (votes >= 15) {
+            return '#4CAF50'
+        } else if (votes >= 12) {
+            return '#8BC34A'    
+        } else if (votes >= 9) {
+            return '#CDDC39'
+        } else if (votes >= 6) {
+            return '#FFEB3B'
+        } else if (votes >= 3) {
+            return '#FFC107'
+        } else if (votes >= 0) {
+            return '#FF9800'
+        } else {
+            return '#F44336'
+        }
+    }
+
+    getEmoji = () => {
+        const {votes} = this.props
+        if (votes >= 15) {
+            return 'em em-rolling_on_the_floor_laughing'
+        } else if (votes >= 12) {
+            return 'em em-laughing'    
+        } else if (votes >= 9) {
+            return 'em em-smiley'
+        } else if (votes >= 6) {
+            return 'em em-slightly_smiling_face'
+        } else if (votes >= 3) {
+            return 'em em-neutral_face'
+        } else if (votes >= 0) {
+            return 'em em-confused'
+        } else {
+            return 'em em-angry'
+        }
+    }
+
     render() {
+        const { handleVote, id, votes, text } = this.props
+        const styles = {
+            border: `2px solid ${this.getColour()}`
+        }
         return (
             <div className="Joke">
                 <div className="Joke-btns">
-                    <div className="arrow" onClick={() => this.props.handleVote(this.props.id, 1)}>
+                    <div className="arrow" onClick={() => handleVote(id, 1)}>
                         <i className="fas fa-arrow-up" />   
                     </div>
-                    <span >{this.props.votes}</span>
-                    <div className="arrow" onClick={() => this.props.handleVote(this.props.id, -1)}>
+                    <span style={styles}>{votes}</span>
+                    <div className="arrow" onClick={() => handleVote(id, -1)}>
                         <i className="fas fa-arrow-down" />
                     </div>
-                    
                 </div>
                 <div className="Joke-text">
-                    <h3>{this.props.text}</h3>
+                    <h3>{text}</h3>
                 </div>
                 <div className="Joke-emoji">
-                <i className="em em-rolling_on_the_floor_laughing"></i>
+                <i className={this.getEmoji()}></i>
                 </div>
-                
             </div>
         )
     }
--- src/JokeList/JokeList.js
+++ src/JokeList/JokeList.js
@@ -15,8 +15,9 @@ class JokeList extends Component {
     }
 
     async componentDidMount () {
+        const { numJokes } = this.props
         let jokes = []
-        for (let i = 0; i < this.props.numJokes; i++) {
+        for (let i = 0; i < numJokes; i++) {
             let response =  await axios.get('https://icanhazdadjoke.com/', 
                 {headers: {Accept: 'application/json'}})
             jokes.push({text: response.data.joke, id: response.data.id, votes: 0})
@@ -36,9 +37,8 @@ class JokeList extends Component {
                 <div className="JokeList-sidebar">
                     <h1 className="JokeList-title"><span>Dad</span> Jokes</h1>
                     <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt=""/>
-                    <button className="JokeList-getmore">New Jokes</button>
+                    {/* <button className="JokeList-getmore">New Jokes</button> */}
                 </div>
-                
                 <div className="JokeList-jokes">
                     {this.state.jokes.map(
                         j => 
